1c8f1e4733b2cbadc2a003d5d564af3a0a8e8637
JakubZebrowski <49155415+jakubzebrowski98@users.noreply.github.com>
2021-03-22 18:17:35 +0100

    add lab 1
    add lab 1

---
+++ FirstTimeWithAzure/FirstApp/Controllers/peopleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using FirstApp.models;
+using System.Linq;
+
+namespace FirstApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class peopleController : ControllerBase
+    {
+
+        private readonly PeopleDB db;
+        public peopleController(PeopleDB db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var people = db.People.ToList();
+            return Ok(people);
+        }
+    }
+}
