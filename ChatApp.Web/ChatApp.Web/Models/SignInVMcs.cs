f6be5690469eed04b52d83cfb0151c62e501f991
JakubZebrowski <49155415+jakubzebrowski98@users.noreply.github.com>
2021-06-21 18:41:43 +0200

    Add files via upload
    add chat app

---
+++ ChatApp.Web/ChatApp.Web/Models/SignInVMcs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Web.Models
+{
+    public class SignInVMcs
+    {
+        [Required(ErrorMessage = "User name is required.")]
+        public string UserName { get; set; }
+    }
+}
